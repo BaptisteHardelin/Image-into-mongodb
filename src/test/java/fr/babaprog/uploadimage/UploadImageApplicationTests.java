cc6add4eb9b18d2457088f4c553f6dcc6741f52a
BaptisteHardelin <baptiste.hardelin@hotmail.com>
2022-06-25 13:38:06 +0200

    first commit

---
+++ src/test/java/fr/babaprog/uploadimage/UploadImageApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.babaprog.uploadimage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadImageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
